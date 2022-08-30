2db2fb0316c3cd6c796408a395b1c6cc8f4d457e
shashikant-98 <77399480+shashikant-98@users.noreply.github.com>
2022-08-31 00:01:23 +0530

    Add files via upload

---
+++ Untitled72.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# * - expression
+# 'hello'- value
+# -87.8 - value
+# - -expression
+# / -expression
+# + -expression
+# 6 -value
+
+
+# In[2]:
+
+
+#Variables : variables are the symbols that we use to store data in a program.
+#Strings : Strings are the data that are filled in variables.
+
+
+# In[3]:
+
+
+#Three different data types are:
+# 1.string : text/number anything bw "" is considered as string data type Ex- "hello","123"
+# 2.integer : All the whole numbers are considered as ineger data type Ex- 7427626
+# 3.float : Decimal numbers are considered as float data type. Ex- 1.33, 4.24
+
+
+# In[4]:
+
+
+#An expression is made up of operates and operands.All the expressions represents value.
+#For example any string is also an expressions since it represents the value of the string as well.
+
+
+# In[ ]:
+
+
+
+
